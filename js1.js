ea9cdc52b477b2d0779d96973955e473173d47b0
Thai Pham <phthai98@gmail.com>
2019-05-01 17:17:21 +0700

    Tìm kiếm trên trang chủ

--- js1.js
+++ js1.js
@@ -13,3 +13,6 @@ $('#backtotop').click(function() {
         scrollTop: 0
     }, 800);
 });
+
+
+(function($){$('#toggle-search').click(function(){$('body').toggleClass('blur');$('.div_search').toggleClass('open');$('.search_').toggleClass('after');$("#ip_search").focus();$(".div_search").animate({height:'60px'});return false;});$(document).click(function(event){var target=$(event.target);if(!target.is('#toggle-search')&&!target.closest('#search-form').size()){$('body').removeClass('blur');$('.div_search').removeClass('open');$('.search_').removeClass('after');$("#ip_search").blur();}});})(jQuery);$('.menu li.active').parents('li').addClass('active');
