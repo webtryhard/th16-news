edfb5871c395e0cd723a8d46ee49e6ce6dc14131
Thai Pham <phthai98@gmail.com>
2019-05-26 09:33:43 +0700

    Chỉnh sửa

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var path=require('path');
-var port=80;
+var port=3000;
  
 var app = express();
 
@@ -10,17 +10,26 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.engine('.hbs', exphbs({
     extname: '.hbs',
-    defaultLayout: 'main.hbs',
     layoutsDir: 'views/layouts'
 }));
 app.set('view engine', '.hbs');
  
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home',{
+        layout:'TrangChu.hbs',
+        title:'Trang chủ',
+        style1:'style1.css',
+        style2: 'style2.css',
+        style3:'login.css',
+        style4:'signup.css',
+        js1: 'jQuery.js',
+        js2:'js.js',
+        logo:'logo.png'
+    });
 });
 
-app.use('/ThoiSu', require('./controllers/ThoiSu.controller'));
-app.use('/ThoiSu/ThoiSuChiTiet', require('./controllers/ThoiSu.controller'));
+var ThoiSu=require('./controllers/ThoiSu.controller');
+app.use('/ThoiSu', ThoiSu);
 
 app.listen(port);
 console.log('http://localhost:' + port);
--- controllers/ThoiSu.controller.js
+++ controllers/ThoiSu.controller.js
@@ -8,29 +8,37 @@ routers.get('/', (req, res) => {
     p.then(rows => {
         //console.log(rows);
         res.render('vwThoiSu/ThoiSu.hbs',{
-            singlenews : rows[0]
+            singlenews : rows[0],
+            layout:'Admin.hbs',
+            title:'Thời sự',
+            style1:'style1.css',
+            style2: 'style2.css',
+            style3:'login.css',
+            style4:'signup.css',
+            js1: 'jQuery.js',
+            js2:'js.js',
+            logo:'logo.png'
         });
     }
     ).catch(err => {
         console.log(err);
     });  
-
-    // var p2 = thoisuModel.getCategory(1);
-    // p2.then(rows => {
-    //     res.render('vwThoiSu/ThoiSuChiTiet.hbs',{
-    //         singlecategories : rows
-    //     });
-    // }
-    // ).catch(err => {
-    //     console.log(err);
-    // });
 })
 
 routers.get('/ThoiSuChiTiet', (req, res) => {
     var p = thoisuModel.getNews(1);
     p.then(rows => {
         res.render('vwThoiSu/ThoiSuChiTiet.hbs',{
-            singlenews : rows[0]
+            singlenews : rows[0],
+            layout:'Admin.hbs',
+            title:'Bài viết',
+            style1:'style1.css',
+            style2: 'style2.css',
+            style3:'login.css',
+            style4:'signup.css',
+            js1: 'jQuery.js',
+            js2:'js.js',
+            logo:'logo.png'
         });
     }
     ).catch(err => {
