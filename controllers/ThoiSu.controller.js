3227394cf8c233f2611783b95d07676f3c20f4f8
thamtran <1612611@student.hcmus.edu.vn>
2019-05-28 09:12:14 +0700

    Load dữ liệu bài viết chi tiết

--- controllers/ThoiSu.controller.js
+++ controllers/ThoiSu.controller.js
@@ -25,21 +25,37 @@ routers.get('/', (req, res) => {
     });  
 })
 
-routers.get('/ThoiSuChiTiet', (req, res) => {
-    var p = thoisuModel.getNews(1);
+routers.get('/ThoiSuChiTiet/:id', (req, res) => {
+    var id = req.params.id;
+    var p = thoisuModel.getNews(id);
     p.then(rows => {
-        res.render('vwThoiSu/ThoiSuChiTiet.hbs',{
-            singlenews : rows[0],
-            layout:'Admin.hbs',
-            title:'Bài viết',
-            style1:'style1.css',
-            style2: 'style2.css',
-            style3:'login.css',
-            style4:'signup.css',
-            js1: 'jQuery.js',
-            js2:'js.js',
-            logo:'logo.png'
-        });
+        var p2 = thoisuModel.getCategoryByNews(rows[0].CatID);
+        p2.then(rows2 => {
+            var p3 = thoisuModel.getWriterByNews(rows[0].Writer_ID);
+            p3.then(rows3 => {
+                var p4 = thoisuModel.getAllTagsByNews(rows[0].News_ID);
+                p4.then(rows4 => {
+                    res.render('vwThoiSu/ThoiSuChiTiet.hbs', {
+                        singlenews: rows[0],
+                        singlecategory: rows2[0],
+                        singlewriter: rows3[0],
+                        tags: rows4,
+                        layout: 'Admin.hbs',
+                        title: 'Bài viết',
+                        style1: 'style1.css',
+                        style2: 'style2.css',
+                        style3: 'login.css',
+                        style4: 'signup.css',
+                        js1: 'jQuery.js',
+                        js2: 'js.js',
+                        logo: 'logo.png'
+                    });
+                }
+                )
+            }
+            )
+        }
+        )
     }
     ).catch(err => {
         console.log(err);
