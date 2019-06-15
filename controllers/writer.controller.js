a82c358bbea9da59b22160c936f1c65192cbadb0
Thai Pham <phthai98@gmail.com>
2019-06-16 01:23:39 +0700

    Thêm chức năng đăng bài viết cho tác giả

--- controllers/writer.controller.js
+++ controllers/writer.controller.js
@@ -1,28 +1,89 @@
 var express = require('express');
+var router = express.Router();
 var writerModel = require('../models/writer.model');
 
 var routers = express.Router();
 
 routers.get('/:id', (req, res) => {
     var id = req.params.id;
-    var page = req.query.page || 1;
-    if (page < 1) page = 1;
-    var limit = 10;
-    var offset = 3 + (page - 1) * limit;
 
-    var p1 = writerModel.getDraft(1);
 
-    Promise.all([p1]).then(([rows1]) => {
-        res.render('vwwriter/writer.hbs', {
+    var p2 = writerModel.getAllNew(id);
+    var p3 = writerModel.singleUser(id);
+
+    Promise.all([p2, p3]).then(([row2, row3]) => {
+        res.render('vwWriter/xemdanhsach.hbs', {
             layout: 'writer.hbs',
             title: 'Writer',
             logo: 'logo.png',
-            style: ['style1.css', 'style2.css', 'writer.css'],
-            js: ['jQuery.js', 'js.js'],
-            draft: rows1,
+            news: row2,
+            writer: row3[0],
         });
     })
 })
 
+routers.get('/:id/add', (req, res) => {
+    var id = req.params.id;
+    var p1 = writerModel.singleUser(id);
+    var p2 = writerModel.getAllNew(id);
+    var p3 = writerModel.getAllCat();
+    Promise.all([p1, p2,p3]).then(([row1, row2,row3]) => {
+        res.render('vwWriter/add.hbs', {
+            new: row2,
+            writer: row1[0],
+            cat:row3,
+            layout: 'writer.hbs',
+            title: 'Đăng bài',
+            logo: 'logo.png',
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+
+})
+
+routers.post('/:id/add', (req, res) => {
+    var id = req.params.id;
+    writerModel.addNew(req.body).then(n => {
+        res.redirect('/writer/' + id);
+    }).catch(err => {
+        console.log(err);
+        res.end('error');
+    })
+})
+
+routers.get('/:ids/edit/:id', (req, res) => {
+    var ids = req.params.ids;
+    var id = req.params.id;
+    var p = writerModel.singleNew(id);
+    var p2 = writerModel.getAllNewState();
+    var p3 = writerModel.getAllCat();
+    var p4 = writerModel.singleUser(ids);
+
+    Promise.all([p, p2, p3,p4]).then(([rows, row2, row3,row4]) => {
+        res.render('vwWriter/edit.hbs', {
+            news: rows[0],
+            newState: row2,
+            writer: row4[0],
+            cat: row3,
+            error: false,
+            layout: 'writer.hbs',
+            title: 'Chỉnh sửa bài viết',
+            logo: 'logo.png',
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+})
+
+routers.post('/:id/update', (req, res) => {
+    var id = req.params.id;
+    writerModel.updateNew(req.body).then(n => {
+        res.redirect('/writer/' + id);
+    }).catch(err => {
+        console.log(err);
+        res.end('error');
+    })
+})
 
 module.exports = routers;
--- models/writer.model.js
+++ models/writer.model.js
@@ -5,5 +5,32 @@ module.exports = {
     getDraft: (CatID) => {
         return db.load(`SELECT * FROM news where CatID = ${CatID} and Deleted = 0 and State_ID = 4 ORDER BY Time DESC LIMIT 0,5`);
     },
-
+    getAllNew: id => {
+        return db.load(`SELECT us.User_ID, us.Username,news.News_ID, news.News_Name, news.Summary,news.Image_Avatar,news.Time,news.Views ,ns.State_ID,ns.State_Name
+        FROM user us JOIN news ON us.User_ID=news.Writer_ID
+        	JOIN categories ct ON news.CatID=ct.CatID
+                    JOIN news_state ns ON news.State_ID=ns.State_ID
+        WHERE us.User_ID=${id}`);
+    },
+    singleUser: id => {
+        return db.load(`SELECT * FROM user WHERE user.User_ID= ${id}`);
+    },
+    singleNew: id => {
+        return db.load(`SELECT *
+        FROM news JOIN categories ct ON news.CatID=ct.CatID
+                    JOIN news_state ns ON news.State_ID=ns.State_ID
+        WHERE news.News_ID=${id}`);
+    },
+    getAllNewState: () => {
+        return db.load(`SELECT * FROM news_state`);
+    },
+    getAllCat: () => {
+        return db.load(`SELECT cat1.CatID, cat1.CatName , cat1.Parent_ID Parent_ID, cat2.CatName Parent_Name, cat1.Deleted FROM categories cat1 LEFT JOIN categories cat2 ON cat1.Parent_ID=cat2.CatID WHERE cat1.Deleted!=1`);
+    },
+    addNew: entity => {
+        return db.add('news', entity);
+    },
+    updateNew: entity => {
+        return db.update('news', 'News_ID', entity);
+    },
 };
